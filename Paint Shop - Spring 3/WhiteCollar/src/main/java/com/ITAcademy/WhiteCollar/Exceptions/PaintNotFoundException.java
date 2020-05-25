ed494ae393245ee9109d9929208cf7a561dd8c4e
Alexandre <amontauber@gmail.com>
2020-05-25 13:58:36 +0200

    Exceptions Shop and Paint completed.

--- Paint Shop - Spring 3/WhiteCollar/src/main/java/com/ITAcademy/WhiteCollar/Exceptions/PaintNotFoundException.java
+++ Paint Shop - Spring 3/WhiteCollar/src/main/java/com/ITAcademy/WhiteCollar/Exceptions/PaintNotFoundException.java
@@ -1,4 +1,9 @@
 package com.ITAcademy.WhiteCollar.Exceptions;
 
-public class PaintNotFoundException {
+public class PaintNotFoundException extends RuntimeException
+{
+    public PaintNotFoundException(Long id)
+    {
+        super("Could not find paint: " + id);
+    }
 }
